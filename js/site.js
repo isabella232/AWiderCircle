966c3ca0995a27c080b36b4a236b353231aa2dbd
Dave Cole <dave@developmentseed.org>
2011-06-03 14:25:05 -0400

    testing IE fix

--- js/site.js
+++ js/site.js
@@ -2,23 +2,27 @@
 
 $(document).ready(function() {		
 	// overlays follow cursor
-	$(document).mousemove(function(e){
-	  $('.wax-tooltip:not(.hidden)').each(function() {
-	  	if($(this).height() + 240 < e.pageY ) {
-	      $(this).offset({
-		      top: e.pageY - $(this).height() - 45,
-		      left: e.pageX - 110
-	      });
-	    } else {
-	    	$(this).addClass('flip');
-	      $(this).offset({
-		      top: e.pageY + 15,
-		      left: e.pageX - 110
-	      });
-	    }
-	  });
-	  $('.olControlZoomPanel div').click(function() {
-	  	$('.wax-tooltip').hide();
-	  });
-	}); 
+	if($.browser.msie) {
+		$('.wax-tooltip').addClass('ie');
+	} else {
+		$(document).mousemove(function(e){
+		  $('.wax-tooltip:not(.hidden)').each(function() {
+		  	if($(this).height() + 240 < e.pageY ) {
+		      $(this).offset({
+			      top: e.pageY - $(this).height() - 45,
+			      left: e.pageX - 110
+		      });
+		    } else {
+		    	$(this).addClass('flip');
+		      $(this).offset({
+			      top: e.pageY + 15,
+			      left: e.pageX - 110
+		      });
+		    }
+		  });
+		  $('.olControlZoomPanel div').click(function() {
+		  	$('.wax-tooltip').hide();
+		  });
+		}); 
+	}
 });

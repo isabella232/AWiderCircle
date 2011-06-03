b88b21e338fdb9a882ac2791ea38d4db1095d4f5
Dave Cole <dave@developmentseed.org>
2011-06-03 11:23:59 -0400

    fixed interactivity overlap and set up flip for overlays that are too high. also removed zip codes (for now)

--- js/site.js
+++ js/site.js
@@ -1,9 +1,21 @@
 // map and interactivity behavoir
 
-$(document).ready(function() {
-			
+$(document).ready(function() {		
 	// overlays follow cursor
 	$(document).mousemove(function(e){
-		$('.wax-tooltip').offset({top: e.pageY - $('.wax-tooltip').height() - 45, left: e.pageX - 110});
+	  $('.wax-tooltip:not(.hidden)').each(function() {
+	  	if($(this).height() + 240 < e.pageY ) {
+	      $(this).offset({
+		      top: e.pageY - $(this).height() - 45,
+		      left: e.pageX - 110
+	      });
+	    } else {
+	    	$(this).addClass('flip');
+	      $(this).offset({
+		      top: e.pageY + 15,
+		      left: e.pageX - 110
+	      });
+	    }
+	  });
 	}); 
 });

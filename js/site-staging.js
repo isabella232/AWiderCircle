690d4240b435e7f5285f0390a1a09625d0f8df87
Dave Cole <dave@developmentseed.org>
2011-07-17 00:54:36 -0400

    wax 3 refactor

--- js/site-staging.js
+++ js/site-staging.js
@@ -1,81 +1,44 @@
-// Dependencies:
-// wax, modestmaps
+var m,d,r,touchPosition,
+    mm = com.modestmaps;
+    url = 'http://a.staging.mapbox.com/awidercircle/1.0.0/'
+      +'mapbox.world-light,awc-world-light-dc,mapbox.dc-bright,awc-addresses-rec,awc-addresses-don,awc-zips'
+      +'/layer.json';
 
-var m,d,r;
-var mm = com.modestmaps;
+function is_touch_device() {  
+  try {  
+    document.createEvent("TouchEvent");  
+    return true;  
+  } catch (e) {  
+    return false;  
+  }  
+}
 
-$(document).ready(function() {
+$(document).ready(function() {   
+  if (!is_touch_device()) {
+    $('body').addClass('desktop');
+  }
 
-	// Build the Map
-
-	m = new mm.Map('map',
-	  new wax.mm.provider({
-	    baseUrl: ['http://a.tiles.mapbox.com/dhcole/',
-	    					'http://b.tiles.mapbox.com/dhcole/',
-	    					'http://c.tiles.mapbox.com/dhcole/',
-	    					'http://d.tiles.mapbox.com/dhcole/'],
-	    layerName: 'world-light-dc,world-light,dc-bright,awc-zips-4',
-	    zoomRange: [0, 16]}),
-	    null,
-	  [
-	  	new mm.DragHandler,
-	  	new mm.DoubleClickHandler,
-	    new mm.TouchHandler
-	  ]);
-	wax.mm.zoomer(m);
-	wax.mm.zoombox(m);
-	wax.mm.interaction(m);
-	m.setCenterZoom(new mm.Location(38.913793178492,-77.012937037187), 14);
-	m.addCallback("zoomed", function(modestmap, e) {
-	  hideOutOfRange(m.getZoom());
+  wax.tilejson(url, function(tilejson) {
+    m = new mm.Map('map',
+      new wax.mm.connector(tilejson),
+      null,
+      [
+        new mm.DragHandler,
+        new mm.DoubleClickHandler,
+        new mm.TouchHandler
+      ]);
+    wax.mm.zoomer(m, tilejson).appendTo(m.parent);
+    wax.mm.zoombox(m, tilejson);
+        wax.mm.interaction(m, tilejson);
+    m.setCenterZoom(new mm.Location(38.913793178492,-77.012937037187), 14);
   });
-	m.addCallback("drawn", function(modestmap, e) {
-	  r.setCenterZoom(m.getCenter(), m.getZoom());
-	  d.setCenterZoom(m.getCenter(), m.getZoom());
-
-  });
-  	
-  r = new mm.Map('map',
-	  new wax.mm.provider({
-	    baseUrl: ['http://a.tiles.mapbox.com/dhcole/',
-	    					'http://b.tiles.mapbox.com/dhcole/',
-	    					'http://c.tiles.mapbox.com/dhcole/',
-	    					'http://d.tiles.mapbox.com/dhcole/'],
-	    layerName: 'awc-addresses-rec-6',
-	    zoomRange: [11, 16]}),
-	    null,null);
-	wax.mm.interaction(r);
-	r.setCenterZoom(new mm.Location(38.913793178492,-77.012937037187), 14);
-
-  d = new mm.Map('map',
-	  new wax.mm.provider({
-	    baseUrl: ['http://a.tiles.mapbox.com/dhcole/',
-	    					'http://b.tiles.mapbox.com/dhcole/',
-	    					'http://c.tiles.mapbox.com/dhcole/',
-	    					'http://d.tiles.mapbox.com/dhcole/'],
-	    layerName: 'awc-addresses-don-6',
-	    zoomRange: [11, 16]}),
-	    null,null);
-	wax.mm.interaction(d);
-	d.setCenterZoom(new mm.Location(38.913793178492,-77.012937037187), 14);
-
-
-  // overlay behavoirs
   
-  var overlays = '',
-  		nOverlays = 1;
-  		
+  // overlay behavoirs 
   $('#map').mousemove(function(e) {
-    $('.wax-tooltip:not(.hidden)').each(function() {
+    $('.wax-tooltip:not(.hidden)').each(function() { 
+      $(this).css('display', 'block');      
 
-			// surpress duplicate overlays
-			if($('.wax-tooltip').length > 1) { 
-				nOverlays = parseInt($('.wax-tooltip').length) - 1;
-			  overlays = '.wax-tooltip:lt('+nOverlays+')';
-				$(overlays).remove();
-			}		
-
-			// follow cursor
+      // follow cursor
       if ($(this).height() + 300 < e.pageY) {
         $(this).offset({
           top: e.pageY - $(this).height() - 45,
@@ -87,43 +50,7 @@ $(document).ready(function() {
           top: e.pageY + 15,
           left: e.pageX - 110
         });
-      }
-			
+      }  
     });
-  }); 
+  });
 });
-
-var hideOutOfRange = function(z) {
-
-  //remove any residual overlays after zooming
-  $('.wax-tooltip').remove();
-
-  //hide layers that don't match this zoom extent
-  if(z<11) {
-	  r.setProvider(new wax.mm.provider({
-	    baseUrl: '',
-	    layerName: '',
-	    filetype: '',
-	    zoomRange: []}));
-	  d.setProvider(new wax.mm.provider({
-	    baseUrl: '',
-	    layerName: '',
-	    filetype: '',
-	    zoomRange: []}));
-  } else {
-	  r.setProvider(new wax.mm.provider({
-	    baseUrl: ['http://a.tiles.mapbox.com/dhcole/',
-	    					'http://b.tiles.mapbox.com/dhcole/',
-	    					'http://c.tiles.mapbox.com/dhcole/',
-	    					'http://d.tiles.mapbox.com/dhcole/'],
-	    layerName: 'awc-addresses-rec-6',
-	    zoomRange: [11, 16]}));
-	  d.setProvider(new wax.mm.provider({
-	    baseUrl: ['http://a.tiles.mapbox.com/dhcole/',
-	    					'http://b.tiles.mapbox.com/dhcole/',
-	    					'http://c.tiles.mapbox.com/dhcole/',
-	    					'http://d.tiles.mapbox.com/dhcole/'],
-	    layerName: 'awc-addresses-don-6',
-	    zoomRange: [11, 16]}));
-  }
-};
